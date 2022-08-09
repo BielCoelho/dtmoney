3325e0ca71613c97f6e145f70a331c67f9fe8611
Gabriel Coelho <gabrielcuei98@gmail.com>
2022-08-09 02:11:36 +0100

    componetizando o webapp e estilizando

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,6 +5,7 @@ export const GlobalStyle = createGlobalStyle`
     --background: #f0f2f5;
     --red: #e52e4d;
     --blue: #5429cc;
+    --green: #33cc95;
     --blue-light: #6933ff;
     --text-title: #363f5f;
     --text-body: #969cb3;
@@ -28,8 +29,7 @@ html {
 }
 
  body {
-
-    background: --var(--background);
+    background: var(--background);
     -webkit-font-smoothing: antialiased;
  }
 
